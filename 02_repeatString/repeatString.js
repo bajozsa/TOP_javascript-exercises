53d463576fd7fac3c9ab1062a62d1b2511db5b43
bajozsa <bajozsa@gmail.com>
2023-02-08 18:31:42 +0100

    2. DONE

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,times) {
+    if (times < 0) {
+        return "ERROR"
+    }
+    var finalStr = "";
+    for (let i = 0; i < times; i++) {
+        finalStr = finalStr.concat("",str)
+    }
+    return finalStr
 };
 
 // Do not edit below this line
