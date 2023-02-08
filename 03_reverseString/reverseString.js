1b06877b20ad654a37cb6a881e68da737f56ecac
bajozsa <bajozsa@gmail.com>
2023-02-08 18:46:59 +0100

    3. DONE

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var finalStr = "";
+    for (let i = str.length; i > 0; i--) {
+        finalStr = finalStr.concat("",str[i-1])
+    }
+    return finalStr
 };
 
 // Do not edit below this line
