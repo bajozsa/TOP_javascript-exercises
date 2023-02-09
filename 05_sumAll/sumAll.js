bce670fec90fbce779466a47d9405f421fcfb9a2
bajozsa <bajozsa@gmail.com>
2023-02-09 14:04:20 +0100

    05 DONE

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || (num1 < 0 || num2 < 0)) return "ERROR";
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    var sum = 0;
+    for (let i = num1-1; i < num2+1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
